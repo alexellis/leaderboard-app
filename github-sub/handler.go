d15755ea9b153f0b37a6265e57884e2100022b24
Alex Ellis <alexellis2@gmail.com>
2019-02-12 23:23:23 +0000

    Add DB connection
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- github-sub/handler.go
+++ github-sub/handler.go
@@ -1,10 +1,42 @@
 package function
 
 import (
+	"database/sql"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
+
+	"github.com/openfaas/openfaas-cloud/sdk"
 )
+
+var db *sql.DB
+
+func init() {
+
+	password, _ := sdk.ReadSecret("password")
+	user, _ := sdk.ReadSecret("username")
+	host, _ := sdk.ReadSecret("host")
+	dbName := os.Getenv("postgres_db")
+	port := os.Getenv("postgres_port")
+	sslmode := os.Getenv("postgres_sslmode")
+
+	connStr := "postgres://" + user + ":" + password + "@" + host + ":" + port + "/" + dbName + "?sslmode=" + sslmode
+
+	var err error
+	db, err = sql.Open("postgres", connStr)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+}
 
 func Handle(w http.ResponseWriter, r *http.Request) {
 	var input []byte
